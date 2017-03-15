b12c6bc85e81a04c154c3be241444f5d7f76bbf0
Rushdie0203 <attractive0203@hotmail.com>
2017-03-15 03:42:05 +0200

    css file path

--- public/themes/pth-clinic/index.php
+++ public/themes/pth-clinic/index.php
@@ -3,7 +3,7 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'themes/pth-clinic/css/style.css';?>">
 <link href="default.css" rel="stylesheet" type="text/css" media="all" />
 
     <meta charset="utf-8">
