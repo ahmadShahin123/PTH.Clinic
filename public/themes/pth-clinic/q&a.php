6191b4d67194bccc404c5dca5ec3dc3db149d226
mmahch <mah_issni@yahoo.com>
2017-04-02 20:10:16 +0300

    simple q&a design php

---
+++ public/themes/pth-clinic/q&a.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Q&A </title>
+
+<link rel="stylesheet" type="text/css" href="q&a.css"/>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+</head>
+<body>
+
+<div class="divborder">
+
+<div class="topdiv">إجابات
+</div>
+
+<div class="imgcontainer"> <img class="imgdiv" src="imgs/avatar.png" alt="doctor image" />
+</div>
+
+
+<div><a class="drname" href=""> د.أحمد</a> :أجاب 
+</div>
+
+<div><a class="question" href="">أعاني من ألم في الظهر، هل يجب أن أتناول الدواء؟</a>
+</div>
+
+<div class="answer">نعم ربما عليك تناول الدواء.
+</div>
+
+<div class="lastdiv"><a class="abutton" href="">اسأل</a></div>
+
+</div>
+
+<div class="divborder">
+
+<div class="topdiv">إجابات
+</div>
+
+<div class="imgcontainer"> <img class="imgdiv" src="imgs/avatar.png" alt="doctor image" />
+</div>
+
+
+<div><a class="drname" href=""> د.أحمد</a> :أجاب 
+</div>
+
+<div><a class="question" href="">أعاني من ألم في الظهر، هل يجب أن أتناول الدواء؟</a>
+</div>
+
+<div class="answer">نعم ربما عليك تناول الدواء.
+</div>
+
+<div class="lastdiv"><a class="abutton" href="">اسأل</a></div>
+
+</div>
+<div class="divborder">
+
+<div class="topdiv">إجابات
+</div>
+
+<div class="imgcontainer"> <img class="imgdiv" src="imgs/avatar.png" alt="doctor image" />
+</div>
+
+
+<div><a class="drname" href=""> د.أحمد</a> :أجاب 
+</div>
+
+<div><a class="question" href="">أعاني من ألم في الظهر، هل يجب أن أتناول الدواء؟</a>
+</div>
+
+<div class="answer">نعم ربما عليك تناول الدواء.
+</div>
+
+<div class="lastdiv"><a class="abutton" href="">اسأل</a></div>
+
+</div>
+<div class="divborder">
+
+<div class="topdiv">إجابات
+</div>
+
+<div class="imgcontainer"> <img class="imgdiv" src="imgs/avatar.png" alt="doctor image" />
+</div>
+
+
+<div><a class="drname" href=""> د.أحمد</a> :أجاب 
+</div>
+
+<div><a class="question" href="">أعاني من ألم في الظهر، هل يجب أن أتناول الدواء؟</a>
+</div>
+
+<div class="answer">نعم ربما عليك تناول الدواء.
+</div>
+
+<div class="lastdiv"><a class="abutton" href="">اسأل</a></div>
+
+</div>
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
