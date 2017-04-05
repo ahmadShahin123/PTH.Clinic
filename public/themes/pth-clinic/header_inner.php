d812eb7e3aafb74f3f61fe0018818018c3f5122e
Rushdie0203 <attractive0203@hotmail.com>
2017-04-05 23:54:04 +0300

    update header_inner

--- public/themes/pth-clinic/header_inner.php
+++ public/themes/pth-clinic/header_inner.php
@@ -2,14 +2,14 @@
 <html lang="en">
 
 <head>
-<link href="https://fonts.googleapis.com/css?family=Mada" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Lalezar" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url() . 'themes/pth-clinic/css/default.css';?>" > 
 
 
-    <title>pth</title>
+    <title>pth.clinic</title>
 
     <!-- Bootstrap Core CSS -->
     <link rel="stylesheet" href="<?php echo base_url() . 'themes/pth-clinic/css/bootstrap.min.css';?>"	>
@@ -31,18 +31,18 @@
             
 <div id="menu" class="font-family" >
     
-	<ul>
+<ul>
 		<li class="active"><a href="<?php echo base_url() . 'index.php' ;?>" class="button" accesskey="1" title="">الرئيسية</a></li>
-		<li><a href="<?php echo base_url() .'themes/pth-clinic/us.php' ;?>"   class="button" accesskey="2" title="">من نحن</a></li>
+		<li><a href="<?php echo base_url() .'index.php/articles/about_us' ;?>"   class="button" accesskey="2" title="">من نحن</a></li>
 		<li><a href="#"  class="button" accesskey="3" title="">مقالات طبية</a></li>
 		<li><a href="<?php echo base_url() .'themes/pth-clinic/q&a.php' ;?>"  class="button" accesskey="4" title="">أسئلة وأجوبة</a></li>
 		<li><a href="#"  class="button" accesskey="5" title="">لياقة بدنية</a></li>
-        <li><a href="<?php echo base_url() .'themes/pth-clinic/contact.php' ;?>"  class="button" accesskey="6" title="">اتصل بنا</a></li>
-		</ul>
-		      <ul>  
-				<li><a href="<?php echo base_url() .'themes/pth-clinic/signUpForm.php' ;?>" class="signin1" accesskey="8" title="">إنشاء حساب</a></li>
-  		<li><a href="<?php echo base_url() .'themes/pth-clinic/signInForm.php' ;?>" class="signin" accesskey="7" title="">سجّل الدخول</a></li>
-		</ul>
+        <li><a href="#"  class="button" accesskey="6" title="">اتصل بنا</a></li>
+</ul>
+	  <ul>  
+		<li><a href="<?php echo base_url() .'themes/pth-clinic/signUpForm.php' ;?>" class="signin1" accesskey="8" title="">إنشاء حساب</a></li>
+  		<li><a href="<?php echo base_url() .'themes/pth-clinic/signInForm.php' ;?>" class="signin" accesskey="7" title="">تسجيل الدخول</a></li>
+      </ul>
    
    </div>
 </div>
