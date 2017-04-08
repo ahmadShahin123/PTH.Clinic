6316729e2abe7b5192b4ae95d4db80803337d289
Rushdie0203 <attractive0203@hotmail.com>
2017-04-09 00:20:11 +0300

    add new files Q&A

---
+++ public/themes/pth-clinic/answer-inner.php
@@ -0,0 +1,206 @@
+<!DOCTYPE php>
+<php dir="rtl" lang="ar">
+<head>
+<meta charset="UTF-8" />
+
+<title>اسئلة واجوبة</title>
+
+
+<meta name="twitter:description" content="موقع متخصص في الإجابة على الإستفسارات الطبية" />
+
+
+
+
+<link rel='stylesheet' id='wpum-frontend-css-css'  href="<?php echo base_url() . 'themes/pth-clinic/css/wp_user_manager_frontend.min.css'; ?>" type='text/css' media='all' />
+<link rel='stylesheet' id='tie-style-css'  href="<?php echo base_url() . 'themes/pth-clinic/css/style_QA.css'; ?>" type='text/css' media='all' />
+
+<link rel='stylesheet' id='jetpack_css-rtl-css'  href="<?php echo base_url() . 'themes/pth-clinic/css/jetpack-rtl.css'; ?>" type='text/css' media='all' />
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/jquery.js'; ?>"></script>
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/jquery-migrate.min.js'; ?>"></script>
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/usp-pro.js'; ?>"></script>
+
+<link rel="stylesheet" href="<?php echo base_url() . 'themes/pth-clinic/css/rtl.css'; ?>" type="text/css" media="screen" />
+
+
+
+
+<style type='text/css'>img#wpstats{display:none}</style>
+<link rel="shortcut icon" href="<?php echo base_url() . 'themes/pth-clinic/images/ask-dr-logo-2.png'; ?>" title="Favicon" />	
+<script type='text/javascript'>
+/* <![CDATA[ */
+var tie = {"prettyPhoto" : "light_rounded" };
+/* ]]> */
+</script>
+
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url() . 'themes/pth-clinic/css/ie8.css'; ?>" />
+
+</head>
+<body id="top" class="rtl home blog">
+<div id="wrapper">
+        <aside id="sidebar">
+                <div class="cover-img">
+                        <div class="cover-body">
+
+                                                        <nav id="main-nav">
+                                        <div class="main-menu"><ul id="menu-main" class="menu">
+                                        <li id="menu-item-61328" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-61328">
+                                        <a href="qa-index.php">التخصصات الباطنية</a>
+<ul class="sub-menu">
+        <li id="menu-item-61490" class="menu-item-61490"><a href="qa-index.php">طب الأسرة</a></li>
+        <li id="menu-item-61489" class="menu-item-61489"><a href="qa-index.php">الباطنة</a></li>
+        <li id="menu-item-61480" class="menu-item-61480"><a href="qa-index.php">الأعصاب والتخدير</a></li>
+        <li id="menu-item-61477" class="menu-item-61477"><a href="qa-index.php">القلب</a></li>
+        <li id="menu-item-61475" class="menu-item-61475"><a href="qa-index.php">الجلدية</a></li>
+        <li id="menu-item-61332" class="menu-item-61332"><a href="qa-index.php">النفسية</a></li>
+        <li id="menu-item-61476" class="menu-item-61476"><a href="qa-index.php">السرطان</a></li>
+        <li id="menu-item-61478" class="menu-item-61478"><a href="qa-index.php">سكّري</a></li>
+</ul>
+</li>
+<li id="menu-item-61333" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-61333"><a href="qa-index.php">التخصصات الجراحية</a>
+<ul class="sub-menu">
+        <li id="menu-item-61484" class="menu-item-61484"><a href="qa-index.php">الجراحة العامة</a></li>
+        <li id="menu-item-61479" class="menu-item-61479"><a href="qa-index.php">العظام</a></li>
+        <li id="menu-item-61335" class="menu-item-61335"><a href="qa-index.php">الأنف والأذن والحنجرة</a></li>
+        <li id="menu-item-61330" class="menu-item-61330"><a href="qa-index.php">الذكورة والمسالك البولية</a></li>
+        <li id="menu-item-61331" class="menu-item-61331"><a href="qa-index.php">العيون</a></li>
+</ul>
+</li>
+<li id="menu-item-61327" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-61327"><a href="qa-index.php">التغذية والرياضة</a>
+<ul class="sub-menu">
+        <li id="menu-item-61481" class="menu-item-61481"><a href="qa-index.php">رياضة ولياقة</a></li>
+        <li id="menu-item-61482" class="menu-item-61482"><a href="qa-index.php">التغذية</a></li>
+</ul>
+</li>
+<li id="menu-item-61473" class="menu-item-61473"><a href="qa-index.php">الأدوية والصيدلية</a></li>
+
+</ul></div>				</nav><!-- .main-nav /-->
+
+
+                                <div class="about-blog-author">
+
+<span class="blog-author-avatar">
+<a href="#">
+<img src=""<?php echo base_url() . 'themes/pth-clinic/images/logo31.png'; ?>" title="" alt="" />
+</a>
+</span>
+
+                                                                                <div class="blog-author-content">
+                                                <h2 class="blog-author-name">اسئلة واجوبة</h2>
+                                                <div>موقع متخصص في الإجابة على الإستفسارات الطبية</div>
+
+                                </div> <!-- .about-blog-author -->				
+                                                                                </div> <!-- .cover-body -->
+                </div> <!-- .cover-img -->
+        </aside><!-- #sidebar /-->
+
+        <aside id="slide-out">
+
+</div><!-- .widget /-->			</aside><!-- #slide-out /-->
+		<div id="main-content" class="container animated">	<div id="content">
+		
+				<article class="item-list post-64087 post type-post status-publish format-standard hentry category-neurology category-ophthalmology tag-5409 tag-5410 tag-3585">
+			
+			
+			<div class="post-inner">
+								    <h1 class="entry-title">ما هو نوع التخدير الذي سيتم عند إخراج الأنبوب من العين؟</h1>
+			
+<p class="post-meta">
+	
+	
+	<!-- <span> في   <a href="#" rel="category tag">الأعصاب والتخدير</a>, <a href="category/ophthalmology/ophthalmology.php" rel="category tag">العيون</a></span>
+	
+	<span> <a href="#.php#comments">‎إجابة واحدة</a></span> -->
+</p>
+<div class="clear"></div>
+	
+	<div class="entry">
+		<div class="usp-images-wrap"></div><p>
+	انا عملة عملية انسداد القناه الدمعيه قبل 11 يوم ووضعوا لي أنبوب سؤالي هل وقت إخراج الأنبوب بيكون التخدير موضعي أو كامل &#8230;.عمري 29 وخايغه من الموضوع</p>
+			</div>
+					
+				
+		</div><!-- .post-inner -->
+		</article><!-- .post-listing -->
+		
+					
+	
+				
+				
+						
+<div id="comments">
+
+<h3 id="comments-title">
+‎إجابة واحدة</h3>
+
+<ol class="commentlist">	<li id="comment-6430">
+		<div  class="comment byuser comment-author-wail even thread-even depth-1 comment-wrap" >
+			<div class="comment-avatar"><img alt='' src='https://secure.gravatar.com/avatar/12882a07dbfaaf8e91641d83a4ef5157?s=65&#038;d=identicon&#038;r=g' srcset='https://secure.gravatar.com/avatar/12882a07dbfaaf8e91641d83a4ef5157?s=130&amp;d=identicon&amp;r=g 2x' class='avatar avatar-65 photo' height='65' width='65' /></div>
+
+			<div class="comment-content">
+				<div class="author-comment">
+					<cite class="fn"><a href='http://Iam-pro.com' rel='external nofollow' class='url'>وائل يار</a></cite> 					<div class="comment-meta commentmetadata"><a href="-d9-85-d8-a7--d9-87-d9-88--d9-86-d9-88-d8-b9--d8.php#comment-6430">	22 مايو, 2015 في 6:44 صباحًا</a></div><!-- .comment-meta .commentmetadata -->
+					<div class="clear"></div>
+				</div>
+			
+									
+				<p dir="rtl">
+	يعتمد ذلك على عمر المريض ومدى تعاونه مع طبيب العيون، ويعتمد كذلك على نوع الأنبوب الذي تم وضعه عند إجراء العملية، لكن بشكل عام، يمكن إجراء عملية إخراج الأنبوب (خاصة الأنبوب الذي يتم وضعه بعد عملية المياه الزرقاء أو الجلوكوما)&nbsp;بالتخدير العام أو بالتخدير الموضعي، أو بدون تخدير حتى، وذلك يعتمد على قرار الطبيب المعالج، هل يريد وضع أنبوب أخر؟ أو إزالته تمامًا؟
+</p>
+<p dir="rtl">
+	&nbsp;
+</p>
+<p dir="rtl">
+	أتمنى لك دوام الصحة والعافية
+</p>
+<p dir="rtl">
+	<a href="http://www.webmd.com/eye-health/tube-shunt-surgery-for-glaucoma" rel="nofollow">المصدر</a></p>
+			</div>
+			<div class="reply"><a rel="nofollow" class="comment-reply-login" href="wp-login.htm">قم بتسجيل الدخول للرد</a></div><!-- .reply -->
+			<div class="comment_author_des">
+				<p class="comment_author_tit">عن المُجيب</p>
+				<p class="author-dec" >طبيب</p>
+				<div class="author_info_data_widget">
+					<ul>
+											<li class="authorsn author-url"><a title="موقعي الشخصي" href="http://Iam-pro.com"></a></li>
+											</ul>
+				</div><!-- .author_info_data -->		
+			</div>
+
+		</div><!-- #comment-##  -->
+
+	</li><!-- #comment-## -->
+</ol>
+    
+
+
+				<div id="respond" class="comment-respond">
+			<h3 id="reply-title" class="comment-reply-title"> <small><a rel="nofollow" id="cancel-comment-reply-link" href="-d9-85-d8-a7--d9-87-d9-88--d9-86-d9-88-d8-b9--d8.php#respond" style="display:none;">‎إلغاء الإجابة</a></small></h3><p class="must-log-in"></p>		</div><!-- #respond -->
+		
+</div><!-- #comments -->
+        	
+			
+		
+
+	</div><!-- .content -->
+	
+		
+	</div><!-- .container /-->
+</div>
+	<div style="display:none">
+	<div class="grofile-hash-map-12882a07dbfaaf8e91641d83a4ef5157">
+	</div>
+	</div>
+	<script type='text/javascript' src='wp-content/js/devicepx-jetpack.js?ver=201710'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wpum_frontend_js = {"ajax":"https:\/\/PTH.Clinic.com\/wp-admin\/admin-ajax.php","checking_credentials":"Checking credentials...","pwd_meter":"","disable_ajax":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/wp_user_manager.min.js'; ?>"></script>
+
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/tie-scripts.js'; ?>"></script>
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/wp-embed.min.js'; ?>"></script>
+
+
+</body>
+</php>
---
+++ public/themes/pth-clinic/qa-index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE php>
+<php dir="rtl" lang="ar">
+<head>
+<meta charset="UTF-8" />
+
+<title>اسئلة واجوبة</title>
+
+
+<meta name="twitter:description" content="موقع متخصص في الإجابة على الإستفسارات الطبية" />
+
+
+
+
+<link rel='stylesheet' id='wpum-frontend-css-css'  href="<?php echo base_url() . 'themes/pth-clinic/css/wp_user_manager_frontend.min.css'; ?>" type='text/css' media='all' />
+<link rel='stylesheet' id='tie-style-css'  href="<?php echo base_url() . 'themes/pth-clinic/css/style_QA.css'; ?>" type='text/css' media='all' />
+
+<link rel='stylesheet' id='jetpack_css-rtl-css'  href="<?php echo base_url() . 'themes/pth-clinic/css/jetpack-rtl.css'; ?>" type='text/css' media='all' />
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/jquery.js'; ?>"></script>
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/jquery-migrate.min.js'; ?>"></script>
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/usp-pro.js'; ?>"></script>
+
+<link rel="stylesheet" href="<?php echo base_url() . 'themes/pth-clinic/css/rtl.css'; ?>" type="text/css" media="screen" />
+
+
+
+
+<style type='text/css'>img#wpstats{display:none}</style>
+<link rel="shortcut icon" href="<?php echo base_url() . 'themes/pth-clinic/images/ask-dr-logo-2.png'; ?>" title="Favicon" />	
+<script type='text/javascript'>
+/* <![CDATA[ */
+var tie = {"prettyPhoto" : "light_rounded" };
+/* ]]> */
+</script>
+
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url() . 'themes/pth-clinic/css/ie8.css'; ?>" />
+
+</head>
+<body id="top" class="rtl home blog">
+<div id="wrapper">
+        <aside id="sidebar">
+                <div class="cover-img">
+                        <div class="cover-body">
+
+                                                        <nav id="main-nav">
+                                        <div class="main-menu"><ul id="menu-main" class="menu">
+                                        <li id="menu-item-61328" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-61328">
+                                        <a href="qa-index.php">التخصصات الباطنية</a>
+<ul class="sub-menu">
+        <li id="menu-item-61490" class="menu-item-61490"><a href="qa-index.php">طب الأسرة</a></li>
+        <li id="menu-item-61489" class="menu-item-61489"><a href="qa-index.php">الباطنة</a></li>
+        <li id="menu-item-61480" class="menu-item-61480"><a href="qa-index.php">الأعصاب والتخدير</a></li>
+        <li id="menu-item-61477" class="menu-item-61477"><a href="qa-index.php">القلب</a></li>
+        <li id="menu-item-61475" class="menu-item-61475"><a href="qa-index.php">الجلدية</a></li>
+        <li id="menu-item-61332" class="menu-item-61332"><a href="qa-index.php">النفسية</a></li>
+        <li id="menu-item-61476" class="menu-item-61476"><a href="qa-index.php">السرطان</a></li>
+        <li id="menu-item-61478" class="menu-item-61478"><a href="qa-index.php">سكّري</a></li>
+</ul>
+</li>
+<li id="menu-item-61333" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-61333"><a href="qa-index.php">التخصصات الجراحية</a>
+<ul class="sub-menu">
+        <li id="menu-item-61484" class="menu-item-61484"><a href="qa-index.php">الجراحة العامة</a></li>
+        <li id="menu-item-61479" class="menu-item-61479"><a href="qa-index.php">العظام</a></li>
+        <li id="menu-item-61335" class="menu-item-61335"><a href="qa-index.php">الأنف والأذن والحنجرة</a></li>
+        <li id="menu-item-61330" class="menu-item-61330"><a href="qa-index.php">الذكورة والمسالك البولية</a></li>
+        <li id="menu-item-61331" class="menu-item-61331"><a href="qa-index.php">العيون</a></li>
+</ul>
+</li>
+<li id="menu-item-61327" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-61327"><a href="qa-index.php">التغذية والرياضة</a>
+<ul class="sub-menu">
+        <li id="menu-item-61481" class="menu-item-61481"><a href="qa-index.php">رياضة ولياقة</a></li>
+        <li id="menu-item-61482" class="menu-item-61482"><a href="qa-index.php">التغذية</a></li>
+</ul>
+</li>
+<li id="menu-item-61473" class="menu-item-61473"><a href="qa-index.php">الأدوية والصيدلية</a></li>
+
+</ul></div>				</nav><!-- .main-nav /-->
+
+
+                                <div class="about-blog-author">
+
+<span class="blog-author-avatar">
+<a href="#">
+<img src=""<?php echo base_url() . 'themes/pth-clinic/images/logo31.png'; ?>" title="" alt="" />
+</a>
+</span>
+
+                                                                                <div class="blog-author-content">
+                                                <h2 class="blog-author-name">اسئلة واجوبة</h2>
+                                                <div>موقع متخصص في الإجابة على الإستفسارات الطبية</div>
+
+                                </div> <!-- .about-blog-author -->				
+                                                                                </div> <!-- .cover-body -->
+                </div> <!-- .cover-img -->
+        </aside><!-- #sidebar /-->
+
+        <aside id="slide-out">
+
+</div><!-- .widget /-->			</aside><!-- #slide-out /-->
+                <div id="main-content" class="container animated">	<div id="content">
+                <article class="item-list post-64087 post type-post status-publish format-standard hentry category-neurology category-ophthalmology tag-5409 tag-5410 tag-3585">
+
+
+                <div class="post-inner">			
+                                <h2 class="entry-title"><a href="answer-inner.php" title="رابط ثابت لـ ما هو نوع التخدير الذي سيتم عند إخراج الأنبوب من العين؟">ما هو نوع التخدير الذي سيتم عند إخراج الأنبوب من العين؟</a></h2>
+
+<p class="post-meta">
+
+
+<!--	<span> في   <a href="#" rel="category tag">الأعصاب والتخدير</a>, <a href="https://askdr.com/category/ophthalmology/ophthalmology.php" rel="category tag">العيون</a></span>
+
+        <span> <a href="#.php#comments">‎إجابة واحدة</a></span> -->
+</p>
+
+
+                </div>
+        </article><!-- .item-list -->
+		
+        </div><!-- .container /-->
+        
+        <div style="display:none">
+        </div>
+
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wpum_frontend_js = {"ajax":"https:\/\/PTH.Clinic.com\/wp-admin\/admin-ajax.php","checking_credentials":"Checking credentials...","pwd_meter":"","disable_ajax":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/wp_user_manager.min.js'; ?>"></script>
+
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/tie-scripts.js'; ?>"></script>
+<script type='text/javascript' src="<?php echo base_url() . 'themes/pth-clinic/js/wp-embed.min.js'; ?>"></script>
+
+
+</body>
+</php>
