c15abbea4714bd99f62fac2c79a6c897ef8d7aa5
Rushdie0203 <attractive0203@hotmail.com>
2017-04-10 22:24:48 +0300

    insert ans_articles.php

---
+++ public/themes/pth-clinic/ans_articles.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+<meta charset="utf-8">
+
+
+<meta property="twitter:image:alt" content="12 مادة غذائية ... تزيد الخصوبة عند السيدات"/><link href="https://www.altibbi.com/global/stylesheets/app_7.min.css" media="screen" rel="stylesheet" type="text/css"/><link href="https://www.altibbi.com/global/stylesheets/articlesviewstyle/articlesviewstyle_1.min.css" media="screen" rel="stylesheet" type="text/css"/>  
+
+<div class="col-md-12 col-sm-12 col-xs-12 text-center pv-mobile ph-xl-mobile">
+<img class="article-image" src="https://www.altibbi.com/global/img/website/xlarge/58beb78925e04article_3699_1.jpg" itemprop='image' alt="12 مادة غذائية ... تزيد الخصوبة عند السيدات">
+</div>
+</div>
+<div id="articleViewPage" class="mojo-container container">
+<div id="right_bar_content" class="col-md-8 col-sm-12 col-xs-12 p0-mobile">
+<div class="box-border p-lg">
+
+<div class="row">
+<div class="col-md-12 col-sm-12 col-xs-12">
+<h1 itemprop='name' class="fn-h1">
+12 مادة غذائية ... تزيد الخصوبة عند السيدات </h1>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12 col-sm-12 col-xs-12">
+<div class="doctor-name">د. لورا</div>
+
+
+</div>
+</div>
+<hr class="mt0">
+<div class="row">
+<div class="col-md-12 col-sm-12 col-xs-12">
+<div class="article-description">
+<div id="m_1" dir="RTL">
+<span id="m_2" lang="AR-SA">يعد موضوع الحمل و الإنجاب ىأكثر المواضيع حساسية في مجتمعاتنا العربية لأن مفهوم العائلة بشكلها أو صورتها الطبيعية تتكون من الأم، الأب والأطفال ومن الصعب تقبل أي شكل أخر لها.</span>
+<span id="m_3" lang="AR-SA">&nbsp;<u></u></span></div>
+<h2 id="mcetoc_1bahhfoqv0" dir="RTL">
+<span lang="AR-SA">عوامل خارجية و داخلية تؤثر على الخصوبة عند المرأة:</span></h2>
+<ul style="list-style-type: disc;">
+<li dir="RTL"><span id="m_4" lang="AR-SA">التوتر و القلق.&nbsp;</span></li>
+<li dir="RTL"><span id="m_5" lang="AR-SA">الملوثات البيئية و المبيدات الحشرية.&nbsp;</span></li>
+<li dir="RTL"><span id="m_6" lang="AR-SA">
+الإشعاعات.&nbsp;</span></li>
+<li dir="RTL"><span id="m_7" lang="AR-SA">إرتفاع متوسط العمر الإنجابي للحمل.&nbsp;</span></li>
+<li dir="RTL"><span id="m_8" lang="AR-SA">الوزن.</span></li>
+<li dir="RTL"><span id="m_9" lang="AR-SA">العادات الغذائية السيئة.</span></li>
+<li dir="RTL"><span id="m_10" lang="AR-SA">تناول بعض الأدوية قد تؤثر على الوظيفة الهرمونية وعلى الإباضة.</span></li></ul>
