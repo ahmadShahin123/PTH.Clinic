aaf1c6f52e73bc04ce2437f22b9e90a7cafbcc9e
Rushdie0203 <attractive0203@hotmail.com>
2017-04-14 22:40:06 +0300

    add ask.php & ask.css

---
+++ public/themes/pth-clinic/ask.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h3>Askdr :</h3>
+
+<div >
+  <form action="#">
+     <select id="art" name="art">
+      <option value="a">#</option>
+      <option value="">#</option>
+      <option value="usa">#</option>
+     </select>
+      <textarea>Some text...</textarea>
+    <input type="submit" value="Submit">
+  </form>
+</div>
+
+</body>
+</html>
