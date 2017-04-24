eafb235a05d0a57a65a87765e440cbd6b64b70ae
Rushdie0203 <attractive0203@hotmail.com>
2017-04-24 22:05:15 +0300

    edit footer style

--- public/themes/pth-clinic/footer.php
+++ public/themes/pth-clinic/footer.php
@@ -49,10 +49,10 @@
 
 				<div class="footer-icons">
 
-					<a href="#"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-linkedin"></i></a>
-					<a href="#"><i class="fa fa-github"></i></a>
+					<a href="#"><i class="fa-facebook"></i></a>
+					<a href="#"><i class="fa-twitter"></i></a>
+					<a href="#"><i class="fa-linkedin"></i></a>
+					<a href="#"><i class="fa-github"></i></a>
 
 				</div>
 
