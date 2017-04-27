b00bb46d3460e578f521708854b451d237f55a12
ahmad <shahin41@live.com>
2017-04-27 03:45:28 +0300

    video

--- application/modules/articles/views/articles_inner.php
+++ application/modules/articles/views/articles_inner.php
@@ -25,7 +25,9 @@
                     <div class="article-description">
                         <div id="m_1" dir="RTL">
                           <?php echo $details->description; ?>
-                            <iframe width="560" height="315" src="<?php echo 'https://www.youtube.com/embed/' . $record->video; ?>"></iframe>
+                            <br/>
+                            <br/>
+                            <iframe width="560" height="315" src="<?php echo 'https://www.youtube.com/embed/' . $details->video; ?>"></iframe>
                         </div>
                     </div>
                 </div>
